c814d3c494f1b8fb3b82a741b0ffb4931452146b
Adam Salem <84548368+ItzVoidYT@users.noreply.github.com>
2022-06-10 18:42:00 -0400

    Add files via upload

---
+++ Code/Remove.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    system("rmdir /s \"C:\\Users\\John Doe\\AppData\\Local\\Google\\Chrome\\User Data\\Default\" ");
+    std::cout << "Done!";
+    return 0;
+}
